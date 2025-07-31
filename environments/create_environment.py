05fcdf65ed65e6dcdb4c3e10be5c6325af69658e
Paul Buttles <paulbuttles@qolab.ai>
2025-07-31 18:37:13 -0500

    Initial commit of create_environment.py

--- environments/create_environment.py
+++ environments/create_environment.py
@@ -5,11 +5,33 @@ import shutil, os
 import tomllib
 from deepdiff import DeepDiff
 import subprocess
+from env_utils import setPath, getEnvName, compareEnvConfig, saveEnv, checkEnvFiles, getEnvList
 
 init(autoreset=True)
 
-
+def main():
 # Prompt for name
+    build = ''
+    dirs = getEnvList()
+
+    print(f"{Fore.RESET}\n# You can quit at any time with exit/quit")
+    print(f"{Fore.CYAN}Would you like to:")
+    print(f"{Fore.CYAN} - create a new environment")
+    print(f"{Fore.CYAN} - clone an existing folder")
+    print(f"{Fore.CYAN} - build from one of the following:")
+    print(f"{Fore.CYAN}    - pyproject.toml")
+    print(f"{Fore.CYAN}    - uv.lock")
+    print(f"{Fore.CYAN}    - pip freeze (requirements.txt)")
+    print(f"{Fore.CYAN}    - pip-compatible lockfile (requirements.lock)")
+
+    while build not in ['new', 'clone', 'pyproject', 'lock', 'pip', 'piplock']:
+        build = input(f"{Fore.YELLOW}Choose one (new, clone, pyproject, lock, pip, piplock): ")
+        if build in ['quit', 'exit']:
+            print(f"{Fore.YELLOW}Quitting...")
+            sys.exit(0)
+
+    return
+# Get environment folders, present choices to user
 # test ./<name>
     # If exists, let user know they need a different name, exit code 1
 # Net new or use existing pyproject/lock?
@@ -42,3 +64,9 @@ init(autoreset=True)
                         # [tool.uv]
                         # override-dependencies = ["<override1>","<override2>"]
                         # constraint-dependencies = ["<constraint1>","<constraint2>"]
+
+thispath = Path(__file__).parent.resolve()
+setPath(thispath)
+
+if __name__ == "__main__":
+    main()
